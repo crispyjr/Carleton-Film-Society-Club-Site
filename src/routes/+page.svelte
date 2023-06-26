44cbf16eef30bc942a1f3943cf7f74c0da36a157
Adrian <adrianalexander17@gmail.com>
2023-06-26 19:37:40 -0400

    sewkgjnaagwag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import logo1 from '$lib/assets/CFS_logo.png'
+    let nav = [
+        {name: 'Sign Up', bg: 'bg-red-500', right: 'right-[-65px]', top: 'top-10'},
+        {name: '', bg: 'bg-black', right: 'right-[-230px]', top: 'top-10'},
+        {name: 'Gallery', bg: 'bg-red-500', right: 'right-24', top: 'top-10'},
+        {bname: 'About', bg: 'bg-red-300', right: 'right-64', top: 'top-10'},
+        {name: 'Events', bg: 'bg-red-200', right: 'right-104', top: 'top-10'},
+    ];
+</script>
+
+<div class="h-screen bg-gray-900">
+    <nav class="relative overflow-hidden bg-white h-[180px] shadow-md flex items-center justify-around">
+        <img src={logo1} class="h-[180px] absolute" alt="Logo">
+        {#each nav as link}
+            <div
+                class="w-[250px] h-[160px] absolute bottom-0 transform skew-x-[-20deg] rotate-110 {nav.right} {nav.top} {nav.bg}">
+                <p class="absolute -rotate-90 right-[-10px] top-20 text-3xl">
+                    {link.name}
+                </p>
+            </div>
+        {/each}
+    </nav>
+
+    <div class="w-full h-[600px] bg-red-500"></div>
+
+    <div class="flex justify-start items-center h-[350px] mt-8 mx-8">
+        <div class="w-1/2 h-full bg-red-300 rounded-lg shadow-lg relative top-8"></div>
+        <div class="w-1/4 h-full bg-red-500 absolute right-8"></div>
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      skew: {
+        20: '20deg',
+     },
+     rotate: {
+        110: '110deg',
+        270: '270deg',
+     },
+    },
   },
   plugins: [],
 }
