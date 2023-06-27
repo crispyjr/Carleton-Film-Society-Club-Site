ab9653360fac451b4749817fe6877b3f780011f6
Shiquana Johnson <56485181+crispyjr@users.noreply.github.com>
2023-06-27 14:58:47 -0400

    made about box

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,26 +2,88 @@
 
 </script>
 
-<h1 class="title" style="font-size: 60px; ">About Us</h1>
-<h2>Welcome to the Carleton Film Society! 
+<div class="aboutSection">
+    <div class="square" style="background: rgb(92, 91, 91); top: 70px;">
+
+    </div>
+    <div class="square" style=" left: 29%; background: rgb(36, 36, 36); top: 60px;">
+
+    </div>
+    <div class="square" style="left: 30%; background: black;">
+        <p class="aboutText">ABOUT</p>
+        <div class="aboutLine"></div>
+        <div class="aboutDescSect"> Welcome to the Carleton Film Society! We’re a passionate community of movie enthusiasts celebrating the art of filmmaking. We’re dedicated to empowering the Carleton community by providing a platform for knowledge-sharing, learning opportunities, and accessible resources to help individuals express and share their unique stories. Collaborate on film projects, enhance your filmmaking skill through workshops and guest speaker presentations, and showcase your short films in our student-led festival.</div>
+    </div>
+</div>
+<div class="teamSection">
+    <div class="meetBar"></div>
+</div>
+
+
+
 
  
-
-    We’re a passionate community of movie enthusiasts celebrating the art of filmmaking. 
-    
-     
-    
-    We’re dedicated to empowering the Carleton community by providing a platform for knowledge-sharing, learning opportunities, and accessible resources to help individuals express and share their unique stories. 
-    
-     
-    
-    Collaborate on film projects, enhance your filmmaking skill through workshops and guest speaker presentations, and showcase your short films in our student-led festival.</h2>
-
-<h1 class="title" style="font-size: 60px;">THE TEAM</h1>    
+<img>
 <style>
+    .meetBar{
+        width:40%;
+        height:20px;
+        background: black;
+        position: relative;
+    }
     .title{
         text-align: center;
         width: 100%;
         position: relative;
+    }
+    .aboutSection{
+        width:100%;
+        height:400px;
+        position: relative;
+    }
+    .teamSection{
+        width:100%;
+        height:400px;
+        background-color: red;
+        position: relative;
+    }
+    .square{
+        width: 40%;
+        height: 300px;
+        background: rgb(36, 36, 36);
+        position: absolute;
+        left:28%;
+        top:50px;
+        color:white;
+
+    }
+    .aboutText{
+        position: relative;
+        font-size: 30px;
+        font-family:'Times New Roman', Times, serif;
+        padding: 10px;
+        padding-left: 20px;
+        
+        
+        
+    }
+    .aboutLine{
+        position: relative;
+        width:93%;
+        height:3px;
+        left:3%;
+        background: white;
+        top:-12px;
+        
+    }
+    .aboutDescSect{
+        position: relative;
+        width:93%;
+        height:70%;
+        background: black;
+        left:3%;
+        top:-7px;
+        font-family: 'Times New Roman', Times, serif;
+        overflow: hidden;
     }
 </style>
