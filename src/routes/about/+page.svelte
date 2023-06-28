eac0890d38a263c8bf2a01f2d33311cb926ea401
Shiquana Johnson <56485181+crispyjr@users.noreply.github.com>
2023-06-28 14:07:22 -0400

    fixed profiles

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,6 +5,7 @@
     import image3 from '$lib/assets/Kim_Headshot3.jpg'
     import image4 from '$lib/assets/Mackenzie_Headshot2.jpg'
     import image5 from '$lib/assets/Beatrice_Headshot2.jpg'
+    import image6 from '$lib/assets/Will_Headshot2.jpg'
     
     
 </script>
@@ -26,40 +27,40 @@
     <div class="meetBar"> <p class="meetText">MEET THE TEAM</p></div>
     <div class="teamFlex">
         <div class="memberFlex">
+            <img class="memberImage" src={image5} alt="lamp">
+            <div class="memberRole"> President</div>
+            <div class="memberName">Beatrice Villadelgado </div>
+            <div class="memberEmail"><p style="">beatricemarievillade@cmail.carleton.ca</p></div>
+        </div>
+        <div class="memberFlex">
+            <img class="memberImage" src={image6} alt="lamp">
+            <div class="memberRole"> Vice President</div>
+            <div class="memberName">Will Osborne </div>
+            <div class="memberEmail"><p style="">willosborne@cmail.carleton.ca</p></div>
+        </div>
+        <div class="memberFlex">
             <img class="memberImage" src={image1} alt="lamp">
-            <div class="memberRole"> Events Coordinator</div>
+            <div class="memberRole">Financial Officer</div>
+            <div class="memberName">Jacob Livingstone</div>
+            <div class="memberEmail"><p style="">JacobLivingstone@cmail.carleton.ca</p></div>
+        </div>
+        <div class="memberFlex">
+            <img class="memberImage" src={image3} alt="lamp">
+            <div class="memberRole">Marketing Officer</div>
+            <div class="memberName">Kimberly Huang </div>
+            <div class="memberEmail"><p style="">Kimhuang@cmail.carleton.ca </p></div>
+        </div>
+        <div class="memberFlex">
+            <img class="memberImage" src={image4} alt="lamp">
+            <div class="memberRole">Marketing Officer</div>
             <div class="memberName">Mackenzie Charbonneau </div>
             <div class="memberEmail"><p style="">mackenziecharbonneau@cmail.carleton.ca</p></div>
         </div>
         <div class="memberFlex">
             <img class="memberImage" src={image2} alt="lamp">
-            <div class="memberRole"> Events Coordinator</div>
-            <div class="memberName">Mackenzie Charbonneau </div>
-            <div class="memberEmail"><p style="">mackenziecharbonneau@cmail.carleton.ca</p></div>
-        </div>
-        <div class="memberFlex">
-            <img class="memberImage" src={image3} alt="lamp">
-            <div class="memberRole"> Events Coordinator</div>
-            <div class="memberName">Mackenzie Charbonneau </div>
-            <div class="memberEmail"><p style="">mackenziecharbonneau@cmail.carleton.ca</p></div>
-        </div>
-        <div class="memberFlex">
-            <img class="memberImage" src={image4} alt="lamp">
-            <div class="memberRole"> Events Coordinator</div>
-            <div class="memberName">Mackenzie Charbonneau </div>
-            <div class="memberEmail"><p style="">mackenziecharbonneau@cmail.carleton.ca</p></div>
-        </div>
-        <div class="memberFlex">
-            <img class="memberImage" src={image5} alt="lamp">
-            <div class="memberRole"> Events Coordinator</div>
-            <div class="memberName">Mackenzie Charbonneau </div>
-            <div class="memberEmail"><p style="">mackenziecharbonneau@cmail.carleton.ca</p></div>
-        </div>
-        <div class="memberFlex">
-            <img class="memberImage" src={image1} alt="lamp">
-            <div class="memberRole"> Events Coordinator</div>
-            <div class="memberName">Mackenzie Charbonneau </div>
-            <div class="memberEmail"><p style="">mackenziecharbonneau@cmail.carleton.ca</p></div>
+            <div class="memberRole">Events Coordinator</div>
+            <div class="memberName">Jarukson Jeevakumar</div>
+            <div class="memberEmail"><p style="">jaruksonjeevakumar@cmail.carleton.ca</p></div>
         </div>
         <div class="memberFlex"></div>
 
