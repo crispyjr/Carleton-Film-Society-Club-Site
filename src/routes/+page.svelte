5344a8bea9bb593edfb9ab6692c2df4ece228afe
Adrian <adrianalexander17@gmail.com>
2023-06-27 21:37:51 -0400

    srhshrjtdjthdh

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<div class="flex flex-col w-full h-screen">
+    <div class="w-screen h-3/4 flex space-x-4 bg-black">
+        <div class="flex-none w-1/6"></div>
+        
+        <!-- <div class="flex-grow bg-blue-300 flex items-center justify-center">
+            <p class="text-white text-center">Your Component</p>
+        </div> -->
+        <div class="flex-none w-1/6"></div>
+    </div>
+    <div class="w-screen bg-red-300 h-1/2">
+        <p class="text-white text-center">Div 2</p>
+    </div>
+    <div class="w-screen bg-green-300 h-1/2">
+        <p class="text-white text-center">Div 3</p>
+    </div>
+</div>
---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,22 @@
+<!-- <script>
+    import Calendar from '@event-calendar/core';
+    import TimeGrid from '@event-calendar/time-grid';
+
+    let ec;
+    let plugins = [TimeGrid];
+    let options = {
+        view: 'timeGridWeek',
+        eventSources: [{events: function() {
+            console.log('fetching...');
+            return [];
+        }}]
+    };
+
+
+    function invokeMethod() {
+        ec.refetchEvents();
+    }
+</script>
+
+<button on:click={invokeMethod}>Refetch events</button>
+<Calendar bind:this={ec} {plugins} {options} /> -->
