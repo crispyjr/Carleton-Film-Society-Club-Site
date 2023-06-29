345c32377c060c0e5720a5ae9eb883ef430a4bdc
Shiquana Johnson <56485181+crispyjr@users.noreply.github.com>
2023-06-28 22:46:03 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
         <Card />
     </div>
     <div class="w-screen bg-gray-900 h-3/4 p-6">
-        <h1 class="text-yellow-400 text-4xl pt-4 pl-4">Canada is our country.</h1>
+        <h1 class="text-yellow-400 text-4xl pt-4 pl-4">Carleton Film Society</h1>
         <p class="text-white pt-4 pl-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
     
