59d05516482b78e853b2365477e80661a6c89f3b
Shiquana Johnson <56485181+crispyjr@users.noreply.github.com>
2023-07-25 01:17:26 -0400

    almost done slideshow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,47 @@
 <script>
   import image1 from '$lib/assets/stock2.jpg';
+   import { images } from './imageData.js';
+  
+    let imageShowIndex = 0;
+    $: console.log(imageShowIndex)
 
+    const prevSlide = () => {
+        console.log("bruh")
+        if(imageShowIndex==0){
+            imageShowIndex = images.length-1;
+        }
+        else{
+            imageShowIndex-=1;
+        }
+    }
+
+    const nextSlide = () => {
+        if(imageShowIndex==images.length-1){
+            imageShowIndex=0;
+        }
+        else{
+            imageShowIndex+=1;
+        }
+    }
 </script>
 
 <div class="slideshow">
-<img class="slideshowImage" src={image1}>
+    {#each images as {id, imgurl}}
+        <img class="slideshowImage" class:showImage={id == imageShowIndex} src={imgurl} alt="oop">
+    {/each}
+
+    <button class="leftArrow" on:click={prevSlide} on:keydown={prevSlide} >&#10094</button>
+    <button class="rightArrow" on:click={nextSlide} on:keydown={nextSlide}>&#10095</button>
+    <div class="dotBar">
+        <div class="dotdotBar">
+            
+            <div class="dotBox">•</div>
+            <div class="dotBox">•</div>
+            <div class="dotBox">•</div>
+        </div>
+    </div>
+
+
 </div>
 <div class="eventsBar"></div>
 
@@ -20,11 +57,59 @@
     .slideshowImage{
         width:100%;
         height:100%;
+        display:none;
+    }
+    .showImage{
+        display: block;
     }
     .eventsBar{
         width:100%;
         height:500px;
-        background: blue;
         position: relative;
+    }
+    .leftArrow{
+        width:3%;
+        height:14%;
+        background: red;
+        position: absolute;
+        top:50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;  
+        font-size: 30px;
+    }
+    .rightArrow{
+        width:3%;
+        height:14%;
+        background: red;
+        position: absolute;
+        top:50%;
+        right:0;
+        display: flex;
+        justify-content: center;
+        align-items: center;  
+        font-size: 30px;
+    }
+    .dotBar{
+        width:100%;
+        height:5%;
+        position: absolute;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+    }
+    .dotdotBar{
+        width:5%;
+        height:100%;
+        display: flex;
+        justify-content: space-around;
+    }
+    .dotBox{
+        width:30%;
+        height:100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 40px;  
     }
 </style>
---
+++ src/routes/imageData.js
@@ -0,0 +1,14 @@
+import image1 from '$lib/assets/stock1.jpg';
+import image2 from '$lib/assets/stock2.jpg';
+import image3 from '$lib/assets/stock3.jpg';
+import image4 from '$lib/assets/stock4.jpg';
+import image5 from '$lib/assets/stock5.jpg';
+
+export const images = [
+    {id:0,
+    imgurl:image1},
+    {id:1,
+    imgurl:image2},
+    {id:2,
+    imgurl:image3}
+];
