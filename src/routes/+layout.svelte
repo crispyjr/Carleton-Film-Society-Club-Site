3aba663e77c1ba573b7efaa041d42e8569f03730
Adrian Alexander <adrianalexander17@gmail.com>
2023-08-26 22:10:28 -0400

    added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script>
-    import "../app.css";
+	import '../app.css';
 	import Navigation from '$lib/Navigation/Navigation.svelte';
-  import Footer from '$lib/Footer/Footer.svelte';
-  </script>
-  
-  <Navigation />
-  <slot />
-  <!-- <Footer /> -->
+	import Footer from '$lib/Footer/Footer.svelte';
+</script>
+
+<Navigation />
+<slot />
+<Footer />
