3ea993a9e2c001fbf29f15dfba4aec56f1419717
Shiquana Johnson <56485181+crispyjr@users.noreply.github.com>
2023-09-11 16:59:57 -0400

    modal!

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,7 +1,6 @@
 <script>
-    import {slide} from 'svelte/transition';
-    import Collapse from './collapse.svelte';
-    import NavCover from '$lib/NavCover.svelte';
+    import {slide} from 'svelte/transition'
+    import Collapse from './collapse.svelte'
 let collapseButton = '˅';
 let visible=true;
 const toggleCollapse = () => {
@@ -17,7 +16,15 @@ const toggleCollapse = () => {
 
 }
 </script>
-<NavCover />
+<div class="navCover"></div>
+<div class="modal">
+    <div class="modalBox">
+        <!-- <div class="time">12:00PM - 12:00PM</div> -->
+        <div class="category">SCREENING</div>
+        <div class="title">EVERYTHING EVERYWHERE ALL AT ONCE</div>
+        <div class="description">The first session involves going through an intro to film/filmmaking, a film-themed Kahoot, ice breaker activities, and a brainstorming session to start thinking of short film ideas you'd like to work on this semester</div>
+    </div>
+</div>
 <div class="eventsTitleBar">
     <div class="eventsTitle">EVENTS</div>
 </div>
@@ -106,7 +113,18 @@ const toggleCollapse = () => {
 </div> -->
 
 
+
+
 <style>
+
+    .navCover{
+        width:100%;
+        height:100px;
+        background: rgb(165, 165, 165);
+        opacity: 0.2;
+        position: relative;
+        box-shadow: 0px 2px 10px red;
+    }
     .eventsTitleBar{
         width:100%;
         height:150px;
@@ -179,7 +197,7 @@ const toggleCollapse = () => {
     text-align: center;
     font-family: 'Koulen';
     
-    color:white;
+    color:black;
 }
 .eventBoxTextBarTop{
     top:20%;
@@ -194,5 +212,61 @@ const toggleCollapse = () => {
     height:70px;
 
 }
-        
+/**MODAL*/
+/* The Modal (background) */
+.modal {
+    background-color: rgba(0, 0, 0, 0.582);
+  position: fixed;
+  width:100%; 
+  height:100%;
+  padding-top: 100px;
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+}
+
+.modalBox{
+    background: #D9D9D9;
+    position: relative;
+    width:40%;
+    height:70%;
+    border-radius: 20px;
+}
+.time{
+    background: red;
+    position:absolute;
+    right:10px;
+    font-size: 40px;
+    font-family: 'Koulen';
+    text-align: right;
+}
+.category{
+    height:120px;
+    position: absolute;
+    left:30px;
+    top:10px;
+    font-family: 'Koulen';
+    font-size: 96px;
+    border-bottom: 5px solid black;
+}
+.title{
+    height:100px;
+    position:absolute;
+    top:125px;
+    left:30px;
+    font-family: 'Koulen';
+    font-size: 40px;
+
+}
+
+.description{
+    height:300px;
+    width:93%;
+    position:absolute;
+    top:220px;
+    left:30px;
+    font-family: 'Koulen';
+    font-size: 25px;
+
+}
 </style>
